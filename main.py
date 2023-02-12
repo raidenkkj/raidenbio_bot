6cc564bd5234cddf2878b99689967bc2f9a4b97e
Raiden Ishigami <contact.raidenishi69@gmail.com>
2023-02-11 21:35:17 -0300

    raidenbot: some fixes
    Signed-off-by: Raiden Ishigami <contact.raidenishi69@gmail.com>

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-RaidenBot = Client(name="useless", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, in_memory=True)
+RaidenBot = Client(name="RaidenBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, in_memory=True)
 
 
 @RaidenBot.on_message(filters.command("start"))
-async def start_message(useless, message):
-    await useless.send_message(message.chat.id, "")
+async def start_message(RaidenBot, message):
+    await RaidenBot.send_message(message.chat.id, "Oi carai")
 
 
 print("Running...")
