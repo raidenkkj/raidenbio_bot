50643ff8879baf76e95bfb8c3256a6e3f45f9d35
Raiden Ishigami <contact.raidenishi69@gmail.com>
2023-03-08 12:37:02 -0300

    Add ban hammer

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
-# Raiden bot
-
 from pyrogram import Client, filters
 from dotenv import load_dotenv
 import os
-
+import re
 
 if os.path.isfile("config.env"):
     load_dotenv("config.env")
-
 
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
+# Define the list of banned usernames
+BANNED_USERNAMES = ["CP", "C P", "C.P"]
+
 RaidenBot = Client(name="RaidenBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, in_memory=True)
 
+@RaidenBot.on_message(filters.new_chat_members)
+async def ban_new_members(RaidenBot, message):
+    for user in message.new_chat_members:
+        if user.username and any(re.search(username, user.username, re.IGNORECASE) for username in BANNED_USERNAMES):
+            await RaidenBot.kick_chat_member(chat_id=message.chat.id, user_id=user.id)
+            await RaidenBot.send_message(chat_id=message.chat.id, text=f"Banned user {user.first_name} ({user.username}) from joining the group.")
 
-@RaidenBot.on_message(filters.command("start"))
-async def start_command_handler(RaidenBot, message):
-    user_id = message.from_user.id
-    username = message.from_user.username
-    
-    log_message = f"{username} ({user_id}) started the bot"
-    await RaidenBot.send_message(-827778569, log_message)
-
-    await message.reply_photo(photo=".images/profile_picture.png", caption="<i>**Hello, this is still in development, please try again later!**</i>")
-
+@RaidenBot.on_message(filters.command("ban"))
+async def ban_command_handler(RaidenBot, message):
+    for arg in message.command[1:]:
+        if arg not in BANNED_USERNAMES:
+            BANNED_USERNAMES.append(arg)
+            await RaidenBot.send_message(chat_id=message.chat.id, text=f"Added username {arg} to the banned list.")
 
 print("Running...")
 RaidenBot.run()
