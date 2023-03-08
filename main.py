2b41091b76a3d3d6131685d1646ea80f24f3337b
Raiden Ishigami <contact.raidenishi69@gmail.com>
2023-03-08 00:44:39 -0300

    add git profile command

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
+import requests
 from pyrogram import Client, filters
 from dotenv import load_dotenv
+import os
 
 
 if os.path.isfile("config.env"):
@@ -23,6 +24,28 @@ async def start_command_handler(RaidenBot, message):
     await RaidenBot.send_message(-827778569, log_message)
 
     await message.reply_photo(photo=".images/profile_picture.png", caption="<i>**Hello, this is still in development, please try again later!**</i>")
+
+
+@RaidenBot.on_message(filters.command("gitprofile"))
+async def gitprofile_command_handler(RaidenBot, message):
+    query = message.text.split(" ", 1)[1]
+    url = f"https://api.github.com/users/{query}"
+    response = requests.get(url)
+    data = response.json()
+
+    if response.status_code == 200:
+        profile_picture_url = data["avatar_url"]
+        name = data["name"]
+        bio = data["bio"]
+        followers = data["followers"]
+        following = data["following"]
+        public_repos = data["public_repos"]
+
+        caption = f"<b>{name}</b>\n<b>Followers:</b> {followers}\n<b>Following:</b> {following}\n<b>Public Repos:</b> {public_repos}"
+
+        await RaidenBot.send_photo(chat_id=message.chat.id, photo=profile_picture_url, caption=caption)
+    else:
+        await message.reply_text("User not found.")
 
 
 print("Running...")
