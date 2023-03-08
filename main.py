0a86be71fe6e85a36227269bcfb5d6b42c5d115b
Raiden Ishigami <contact.raidenishi69@gmail.com>
2023-03-08 12:52:42 -0300

    Add ban list

--- main.py
+++ main.py
@@ -30,5 +30,34 @@ async def ban_command_handler(RaidenBot, message):
             BANNED_USERNAMES.append(arg)
             await RaidenBot.send_message(chat_id=message.chat.id, text=f"Added username {arg} to the banned list.")
 
+@RaidenBot.on_message(filters.command("ban_list"))
+async def ban_command_handler(RaidenBot, message):
+    # Create a list of banned usernames as clickable buttons
+    banned_usernames_list = []
+    for banned_username in BANNED_USERNAMES:
+        banned_usernames_list.append([InlineKeyboardButton(banned_username, callback_data=f"unban_{banned_username}")])
+    # Create a markup object to display the banned usernames list
+    markup = InlineKeyboardMarkup(banned_usernames_list)
+    # Send the list of banned usernames as a message
+    await RaidenBot.send_message(chat_id=message.chat.id, text="List of banned usernames:", reply_markup=markup)
+
+@RaidenBot.on_callback_query()
+async def callback_query_handler(RaidenBot, query):
+    # Check if the query is a request to unban a user
+    if query.data.startswith("unban_"):
+        banned_username = query.data.split("_")[1]
+        if banned_username in BANNED_USERNAMES:
+            # Remove the banned username from the list
+            BANNED_USERNAMES.remove(banned_username)
+            # Edit the original message with the updated banned usernames list
+            banned_usernames_list = []
+            for banned_username in BANNED_USERNAMES:
+                banned_usernames_list.append([InlineKeyboardButton(banned_username, callback_data=f"unban_{banned_username}")])
+            markup = InlineKeyboardMarkup(banned_usernames_list)
+            await RaidenBot.edit_message_reply_markup(chat_id=query.message.chat.id, message_id=query.message.message_id, reply_markup=markup)
+            # Send a confirmation message
+            await RaidenBot.answer_callback_query(query.id, text=f"Unbanned {banned_username}")
+
+
 print("Running...")
 RaidenBot.run()
